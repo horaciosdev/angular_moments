30807ec491647b4225b675b6d6ede08bcb77f5ef
Horacio <horacio.schumann@gmail.com>
2022-12-06 22:36:35 -0300

    Criado formulário de criação de moments, criadas interfaces, adicionada validação do formulário.

---
+++ src/app/Comments.ts
@@ -0,0 +1,9 @@
+export interface Comment {
+    id?: number;
+    text: string;
+    username: string;
+    mommentId: number;
+    created_at?: string;
+    udated_at?: string;
+
+}
---
+++ src/app/components/moment-form/moment-form.component.ts
@@ -0,0 +1,37 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-moment-form',
+  templateUrl: './moment-form.component.html',
+  styleUrls: ['./moment-form.component.scss']
+})
+export class MomentFormComponent implements OnInit {
+  @Input() btnText!: string;
+
+  momentForm!: FormGroup;
+  
+  ngOnInit(): void {
+    this.momentForm = new FormGroup({
+      id: new FormControl(''),
+      title: new FormControl('', [Validators.required]),
+      description: new FormControl('', [Validators.required]),
+      image: new FormControl('')
+    });
+  }
+
+  get title(){
+    return this.momentForm.get('title')!;
+  }
+
+  get description(){
+    return this.momentForm.get('description')!;
+  }
+
+  submit() {
+    if(this.momentForm.invalid) {
+      return;
+    }
+    console.log("Enviou formulário");
+  }
+}
