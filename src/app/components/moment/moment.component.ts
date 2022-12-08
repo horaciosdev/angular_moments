9eb83103063aa0ecc110e23eac2b6db6cda9047c
Horacio <horacio.schumann@gmail.com>
2022-12-08 14:41:04 -0300

    Adicionada funcionalidade de exclusão de um moment

--- src/app/components/moment/moment.component.ts
+++ src/app/components/moment/moment.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { MomentService } from 'src/app/services/moment.service';
+import { MessagesService } from 'src/app/services/messages.service';
 import { Moment } from 'src/app/Moment'
 import { environment } from 'src/app/environments/environment';
 import { faTimesCircle, faEdit } from '@fortawesome/free-regular-svg-icons';
@@ -19,12 +20,22 @@ export class MomentComponent {
 
   constructor(
     private momentService: MomentService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private router: Router,
+    private messagesService: MessagesService
   ) {}
 
   ngOnInit():void{
     const id = Number(this.route.snapshot.paramMap.get('id'));
 
     this.momentService.getMoment(id).subscribe( (item) => (this.moment = item.data));
+  }
+
+  async removeHandler(id: number){
+    await this.momentService.removeMoment(id).subscribe()
+
+    this.messagesService.add("Momento excluído com sucesso!");
+    
+    this.router.navigate(['/']);
   }
 }
--- src/app/services/moment.service.ts
+++ src/app/services/moment.service.ts
@@ -28,4 +28,9 @@ export class MomentService {
   createMoment(formData: FormData): Observable<FormData> {
     return this.http.post<FormData>(this.apiUrl, formData);
   }
+
+  removeMoment(id: number){
+    const url = `${this.apiUrl}/${id}`;
+    return this.http.delete(url);
+  }
 }
